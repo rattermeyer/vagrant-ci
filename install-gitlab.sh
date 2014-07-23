bc8d8b75ee3a644777f4c32d11be711f52c46555
Richard Attermeyer <richard.attermeyer@gmail.com>
2014-07-23 21:39:15 +0200

    updated to gitlab 7.1

--- install-gitlab.sh
+++ install-gitlab.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 apt-get update
 apt-get upgrade -y
-apt-get install -y git curl openssh-server
+apt-get install -y git curl openssh-server 
 DEBIAN_FRONTEND=noninteractive apt-get install -y postfix
-curl -k -s https://downloads-packages.s3.amazonaws.com/ubuntu-14.04/gitlab_7.0.0-omnibus-1_amd64.deb > gitlab_7.0.0-omnibus-1_amd64.deb
-dpkg -i gitlab_7.0.0-omnibus-1_amd64.deb
+curl -k -s https://downloads-packages.s3.amazonaws.com/ubuntu-14.04/gitlab_7.1.0-omnibus-1_amd64.deb > gitlab_7.0.0-omnibus-1_amd64.deb
+dpkg -i gitlab_7.1.0-omnibus-1_amd64.deb
 gitlabctl reconfigure
