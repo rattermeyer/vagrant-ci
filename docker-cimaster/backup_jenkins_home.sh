f291fd40e9840c87dd6435cc445dec6e0c3610fe
Richard Attermeyer <richard.attermeyer@gmail.com>
2014-09-27 21:03:45 +0000

    removed certain dirs

--- docker-cimaster/backup_jenkins_home.sh
+++ docker-cimaster/backup_jenkins_home.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 IP_ADDRESS=`docker inspect $1 | grep "IPAddress"  | cut -d: -f2 | sed -e 's/"//g' -e 's/ //g' -e 's/,//g'`
 echo ${IP_ADDRESS}
-ssh -i vagrant_key vagrant@${IP_ADDRESS} sudo -u jenkins tar -C /var/lib/jenkins --exclude='.ssh' --exclude='workspace' --exclude='builds' --exclude='./plugins' --exclude='./.m2' --exclude='./.jenkins' --exclude='./tools' --exclude='cache' -czvf /tmp/jenkins_home.tar.gz .
+ssh -i vagrant_key vagrant@${IP_ADDRESS} sudo -u jenkins tar -C /var/lib/jenkins --exclude='.ssh' --exclude='workspace' --exclude='builds' --exclude='./plugins' --exclude='./.m2' --exclude='./.jenkins' --exclude='./tools' --exclude='cache' --exclude='secrets' --exclude='fingerprints' -czvf /tmp/jenkins_home.tar.gz .
 scp -i vagrant_key vagrant@${IP_ADDRESS}:/tmp/jenkins_home.tar.gz .
 
