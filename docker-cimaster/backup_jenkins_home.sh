6a36f3e782e6934255c44064bf8f4dd50a74b8f3
Richard Attermeyer <richard.attermeyer@gmail.com>
2014-10-05 14:21:47 +0200

    massive commit

--- docker-cimaster/backup_jenkins_home.sh
+++ docker-cimaster/backup_jenkins_home.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 IP_ADDRESS=`docker inspect $1 | grep "IPAddress"  | cut -d: -f2 | sed -e 's/"//g' -e 's/ //g' -e 's/,//g'`
 echo ${IP_ADDRESS}
-ssh -i vagrant_key vagrant@${IP_ADDRESS} sudo -u jenkins tar -C /var/lib/jenkins --exclude='.ssh' --exclude='workspace' --exclude='builds' --exclude='./plugins' --exclude='./.m2' --exclude='./.jenkins' --exclude='./tools' --exclude='cache' --exclude='secrets' --exclude='fingerprints' -czvf /tmp/jenkins_home.tar.gz .
+#ssh -i vagrant_key vagrant@${IP_ADDRESS} sudo -u jenkins tar -C /var/lib/jenkins --exclude='.ssh' --exclude='workspace' --exclude='builds' --exclude='./plugins' --exclude='./.m2' --exclude='./.jenkins' --exclude='./tools' --exclude='cache' --exclude='secrets' --exclude='fingerprints' -czvf /tmp/jenkins_home.tar.gz .
+ssh -i vagrant_key vagrant@${IP_ADDRESS} sudo -u jenkins tar -C /var/lib/jenkins --exclude='.ssh' --exclude='workspace' --exclude='builds' --exclude='./plugins' --exclude='./.m2' --exclude='./.jenkins' --exclude='./tools' --exclude='cache' --exclude='fingerprints' --exclude='config-history' --exclude='logs' --exclude='.java' --exclude='users' -czvf /tmp/jenkins_home.tar.gz .
 scp -i vagrant_key vagrant@${IP_ADDRESS}:/tmp/jenkins_home.tar.gz .
 
